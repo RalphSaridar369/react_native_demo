3b9e0af08b61c238e7c4478730893ca80031c28b
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-01-17 09:57:37 +0200

    Used useContext in home to check if it works correctly

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useContext } from 'react'
 import {View,Text} from 'react-native';
-import { useContext } from 'react';
 import { MainContext } from '../../MainContext';
 
 const Home = ({navigation}) =>{
+    const {LoggedIn} = useContext(MainContext);
+    useEffect(()=>{
+
+    },[])
     return(
         <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+            <Text>{LoggedIn?"logged in":"not logged in"}</Text>
             <Text onPress={()=>navigation.navigate("homeDetailed")}>Home Page</Text>
         </View>
     )
