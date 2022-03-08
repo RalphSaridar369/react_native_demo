3cef19b6e07d8d6f0da5e53487628ebde533be88
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-03-08 19:19:29 +0200

    Adding products

--- mockData.js
+++ mockData.js
@@ -16,29 +16,11 @@ export const products = [
     {id:15, name:"Ps5", price:800, image:require("./assets/Products/product_15.png")},
 ]
 
-export const flatList1 = [
-    {id:1, name:"Laptop Samsung", price:800, image:require("./assets/Products/product_1.png")},
-    {id:2, name:"Macbook Laptop", price:1600, image:require("./assets/Products/product_2.png")},
-    {id:3, name:"Dell Laptop", price:900, image:require("./assets/Products/product_3.png")},
-    {id:4, name:"Lenovo Thinkpad Laptop", price:1000, image:require("./assets/Products/product_4.png")},
-    {id:5, name:"Asus Gaming Laptop", price:1800, image:require("./assets/Products/product_5.png")},
-]
+export const flatList1 = products.slice(0,5);
 
-export const flatList2 = [
-    {id:6, name:"Iphone 6s", price:500, image:require("./assets/Products/product_6.png")},
-    {id:7, name:"Iphone x", price:700, image:require("./assets/Products/product_7.png")},
-    {id:8, name:"Iphone 13 pro", price:1200, image:require("./assets/Products/product_8.png")},
-    {id:9, name:"Samsung Galaxy Fold", price:1300, image:require("./assets/Products/product_9.png")},
-    {id:10, name:"Samsung Galaxy s10", price:900, image:require("./assets/Products/product_10.png")},
-]
+export const flatList2 = products.slice(5,10);
 
-export const flatList3 = [
-    {id:11, name:"Xbox 360", price:300, image:require("./assets/Products/product_11.png")},
-    {id:12, name:"Xbox One", price:500, image:require("./assets/Products/product_12.png")},
-    {id:13, name:"PS3", price:300, image:require("./assets/Products/product_13.png")},
-    {id:14, name:"PS4", price:400, image:require("./assets/Products/product_14.png")},
-    {id:15, name:"PS5", price:800, image:require("./assets/Products/product_15.png")},
-]
+export const flatList3 = products.slice(10,15);
 
 export const carouselData = [
     {id:1, name:"Laptop Samsung", price:800, image:require("./assets/Products/product_1.png")},
