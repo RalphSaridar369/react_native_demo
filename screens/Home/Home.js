207e6027c670a060f9e9047f45dbe0b96be276dc
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-03-08 16:42:58 +0200

    Working on home product but not working so far on ios will recheck it

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -4,15 +4,15 @@ import CarouselComponent from './components/Carousel';
 import { carouselData, products } from '../../mockData';
 import FlatlistComponent from './components/Flatlist';
 
-const Home = () => {
+const Home = ({navigation}) => {
     return (
         <View style={{ flex: 1, backgroundColor:'#fff'}}>
             <ScrollView>
                 <CarouselComponent data={carouselData} loop={true} autoplay={true} enableMomentum autoplayInterval={5000}/>
                 <View style={{marginTop:20}}>
-                    <FlatlistComponent headerText="Top Products" data={products}/>
-                    <FlatlistComponent headerText="Best Selling" data={products}/>
-                    <FlatlistComponent headerText="Top Deals" data={products}/>
+                    <FlatlistComponent headerText="Top Products" data={products} navigation={navigation}/>
+                    <FlatlistComponent headerText="Best Selling" data={products} navigation={navigation}/>
+                    <FlatlistComponent headerText="Top Deals" data={products} navigation={navigation}/>
                 </View>
             </ScrollView>
         </View>
