0a347fd3bdfbd6834f4de2b0022aa0f42f95ff6b
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-03-12 15:54:59 +0200

    Done with dashboard

--- screens/Dashboard/Dashboard.js
+++ screens/Dashboard/Dashboard.js
@@ -1,13 +1,41 @@
-import {ScrollView, View} from 'react-native';
-import {styles} from './styles';
+import { ScrollView, View } from 'react-native';
+import { styles } from './styles';
+import { LinearGradient } from 'expo-linear-gradient';
+import { BarChart, Grid, LineChart } from 'react-native-svg-charts'
+import { Text } from '../../components';
 
-const Dashboard =()=>{
-    return(
-        <View style={styles.main}>
-            <ScrollView showsVerticalScrollIndicator={false}>
+const Dashboard = () => {
 
+    const fill_bar = '#FF6863'
+    const data_bar = [50, 10, 40, 95, -4, -24, null, 85, undefined, 0, 35, 53, -53, 24, 50, -20, -80]
+    const data_line = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
+
+    return (
+        <LinearGradient
+            // Background Linear Gradient
+            colors={['#FF6863', '#fff', '#fff']}
+            style={{ flex: 1 }}
+        >
+            <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollview}>
+                <View style={styles.card}>
+                    <Text style={styles.card_header}>Sales</Text>
+                    <BarChart style={{ height: 200 }} data={data_bar} svg={{ fill_bar }} contentInset={{ top: 30, bottom: 30 }}>
+                        <Grid />
+                    </BarChart>
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.card_header}>Sales per week</Text>
+                    <LineChart
+                        style={{ height: 200 }}
+                        data={data_line}
+                        svg={{ stroke: '#FF6863' }}
+                        contentInset={{ top: 20, bottom: 20 }}
+                    >
+                        <Grid />
+                    </LineChart>
+                </View>
             </ScrollView>
-        </View>
+        </LinearGradient>
     )
 }
 
--- screens/Dashboard/styles.js
+++ screens/Dashboard/styles.js
@@ -1,7 +1,20 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    main:{
-        flex:1
-    }
+    scrollview:{
+        padding:20,
+        marginBottom:40,
+    },
+    card:{
+        backgroundColor:'#fff',
+        padding:20,
+        marginVertical:20,
+        marginBottom:20,
+        borderWidth:1,
+        borderColor:'lightgray',
+        borderRadius:20
+    },
+    card_header:{
+        fontSize:24  
+    },
 })
